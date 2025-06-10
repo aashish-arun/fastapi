e58850d470da98acf7e0fc9487f7434151d796c7
aashish-arun <aashish.ouo@gmail.com>
2025-06-09 23:28:00 -0600

    FastAPI vs Flask

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ def create_item(item:Item): # This is the cmd used instead of curl in windows fo
 def list_items(limit:int=10): # This is the cmd used instead of curl in windows for post requests "Invoke-WebRequest -Uri "http://localhost:8000/items?limit=item_limit""
     return items[0:limit]
 
-
 @app.get("/items/{item_id}", response_model=Item) 
 def get_item(item_id:int) -> Item: # This is the cmd used instead of curl in windows for get requests "Invoke-WebRequest -Uri "http://localhost:8000/items/item_id""
     if item_id < len(items):
         return items[item_id]
     else:
         raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+    
+# Main reason to use FastAPI over FLASK is that it is async by default
